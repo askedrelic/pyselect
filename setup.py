157d5f932d89abefb491caade959bead8c7a53d8
Matt Behrens <askedrelic@gmail.com>
2013-11-03 15:33:58 -0800

    updating setup.py formatting

--- setup.py
+++ setup.py
@@ -4,20 +4,22 @@ from setuptools import setup, find_packages
 import pyselect
 
 setup(
-    name             = 'pyselect',
-    version          = pyselect.__version__,
-    description      = '',
-    long_description = open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    name                 = 'pyselect',
+    version              = pyselect.__version__,
+    description          = 'A Python library for easily getting user input form multiple items in a list, emulating the Bash(1) builtin select.',
+    long_description     = open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
 
-    author           = pyselect.__author__,
-    author_email     = 'askedrelic@gmail.com',
-    url              = 'https://github.com/askedrelic/pyselect',
-    license          = open("LICENSE.txt").read(),
+    author               = pyselect.__author__,
+    author_email         = 'askedrelic@gmail.com',
+    url                  = 'https://github.com/askedrelic/pyselect',
+    license              = open("LICENSE.txt").read(),
 
-    packages         = ['pyselect'],
-    # test_suite       = 'tests',
+    # packages           = ['pyselect'],
+    py_modules           = ['pyselect'],
+    include_package_data = True,
+    # test_suite         = 'tests',
 
-    classifiers      = (
+    classifiers          = (
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
